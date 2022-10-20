a72f1b415e0e55131e9633cac1604754fc31c4b4
YanNezamutdinov <nezamutdinovyan@gmail.com>
2022-10-20 18:02:12 +0300

    6

--- job.py
+++ job.py
@@ -1,9 +1,21 @@
-from threading import Thread
-
+def coroutine(f):
+    def wrap(*args, **kwargs):
+        gen = f(*args, **kwargs)
+        gen.send(None)
+        return gen
+    return wrap
 
 class Job:
-    def __init__(self, start_at="", max_working_time=-1, tries=0, dependencies=[]):
-        print(start_at)
+    def __init__(self, name=None, start_at="", max_working_time=-1, tries=0, dependencies=[]):
+        self.name = name
+        self.start_at = start_at
+        self.max_working_time = max_working_time
+        self.tries = tries
+        self.dependencies = dependencies
+
+    def __call__(self, *args, **kwargs):
+        print(self.name)
+
 
     def run(self):
         pass
--- scheduler.py
+++ scheduler.py
@@ -1,9 +1,7 @@
 import json
-import threading
 import uuid
 from concurrent.futures._base import as_completed
 from concurrent.futures.thread import ThreadPoolExecutor
-from queue import Queue
 
 from job import Job
 
@@ -21,42 +19,29 @@ class Scheduler:
         self.pool_size = pool_size
 
     def schedule(self, **kwargs):
-        # with open('myfile.txt', 'a') as f:
-        #     line = f'{uuid.uuid4()}: {kargs}'
-        #     f.write(line)
-        #     f.write('\n')
-        for key in kwargs:
-            print(key)
-
         with open('myfile.json', 'r') as f:
             try:
                 data = json.load(f)
             except:
-                data = list()
-        data.append({str(uuid.uuid4()): dict(kwargs)})
+                data = dict()
+        data[str(uuid.uuid4())] = kwargs
 
         with open('myfile.json', 'w') as f:
             json.dump(data, f)
-
-
-        # data = {line.strip() for line in open("myfile.txt", 'r')}
-
 
     @coroutine
     def run(self):
         try:
             while True:
-                for line in open("myfile.json", 'r'):
-                    print(line.get('start_at'))
-                # data = {dict(line) for line in open("myfile.json", 'r')}
-                with ThreadPoolExecutor(max_workers=self.pool_size) as pool:
-                    # for item in data:
-                    #     print(item.get('start_at'))
+                with open("myfile.json", 'r') as f:
+                    data = json.load(f)
+                with ThreadPoolExecutor(max_workers=self.pool_size * 2) as pool:
+                    future_to_url = {pool.submit(Job, name=key, **val): val for key, val in data.items()}
                     # futures = []
                     # futures.append(pool.submit(Job, ))
-                    # for future in as_completed(futures):
-                    #     print(future.result())
-                    pass
+                    for future in as_completed(future_to_url):
+                        print(future.result())
+                    #     print(future.)
         except GeneratorExit:
             pass
 
